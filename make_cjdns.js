00998ac331ab6e34e6c3d35d5564cfccfcc34b32
Thomas Suckow <tsuckow@gmail.com>
2018-05-01 09:29:05 -0700

    Get stuff compiling

---
+++ make_cjdns.js
@@ -0,0 +1,19 @@
+var fs = require('fs');
+var p = require("path")
+var deleteFolderRecursive = function(path) {
+  if (fs.existsSync(path)) {
+    fs.readdirSync(path).forEach(function(file, index){
+      var curPath = p.join(path, file);
+      if (fs.lstatSync(curPath).isDirectory()) { // recurse
+        deleteFolderRecursive(curPath);
+      } else { // delete file
+        fs.unlinkSync(curPath);
+      }
+    });
+    fs.rmdirSync(path);
+  }
+};
+
+deleteFolderRecursive('build_linux');
+
+require('./cjdns-src/node_build/make.js');
